30762d4863bfb2ad20bbdaae95d3e556900539b9
Winexnix <101360465+Winexnix@users.noreply.github.com>
2022-06-07 01:41:13 -0400

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,6 @@
+
+const menu_btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.wmenu');
+    menu_btn.addEventListener('click',()=>{
+        menu.classList.toggle('active')
+    })
